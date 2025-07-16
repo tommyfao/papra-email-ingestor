acd3b99f365ef0884adef0db4bf00e919bb99f3d
Thomas Faour <thomas@faour.cc>
2025-07-15 21:39:38 -0400

    remove email-parser

--- src/imap_client.py
+++ src/imap_client.py
@@ -1,4 +1,6 @@
 import imaplib
+import email
+from email.header import decode_header
 
 class ImapClient:
     def __init__(self, host, username, password, port=993):
@@ -22,8 +24,42 @@ class ImapClient:
         emails = []
         for email_id in email_ids:
             status, msg_data = self.connection.fetch(email_id, '(RFC822)')
-            emails.append(msg_data[0][1])
+            raw_email = msg_data[0][1]
+            parsed_email = email.message_from_bytes(raw_email)
+            emails.append(self.parse_email(parsed_email))
         return emails
+
+    def parse_email(self, msg):
+        """Parse email message and extract key information"""
+        subject = self.decode_header_value(msg.get("Subject", ""))
+        sender = msg.get("From", "")
+        date = msg.get("Date", "")
+        
+        # Extract body
+        body = ""
+        if msg.is_multipart():
+            for part in msg.walk():
+                if part.get_content_type() == "text/plain":
+                    body = part.get_payload(decode=True).decode('utf-8', errors='ignore')
+                    break
+        else:
+            body = msg.get_payload(decode=True).decode('utf-8', errors='ignore')
+        
+        return {
+            'subject': subject,
+            'sender': sender,
+            'date': date,
+            'body': body
+        }
+
+    def decode_header_value(self, value):
+        """Decode email header values"""
+        if value:
+            decoded_value, encoding = decode_header(value)[0]
+            if isinstance(decoded_value, bytes):
+                return decoded_value.decode(encoding or 'utf-8', errors='ignore')
+            return decoded_value
+        return ""
 
     def disconnect(self):
         if self.connection:
